eda23ee0f853c8f64a11afd3e5bcd9ebfa9d2aa3
Alec Eiber <aleceiber@gmail.com>
2022-05-19 23:46:16 -0400

    added smooth scrolling

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -5,7 +5,7 @@ import ProjectItem from './ProjectItem';
 
 const Projects = () => {
   return (
-    <div className="w-full">
+    <div id="projects" className="w-full">
       <div className="max-w-[1240] mx-auto px-2 py-16">
         <p className="text-xl tracking-widest uppercase text-[#21568a]">
           Projects
---
+++ pages/tiger.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const tiger = () => {
+  return <div>tiger</div>;
+};
+
+export default tiger;
