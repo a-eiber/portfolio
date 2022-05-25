8942e9c02dca55c69daf528f3854abf43b82f4a6
Alec Eiber <aleceiber@gmail.com>
2022-05-25 12:01:47 -0400

    updated projects page

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -6,34 +6,36 @@ import ProjectItem from './ProjectItem';
 
 const Projects = () => {
   return (
-    <div id="projects" className="w-full p-2 pt-8">
-      <div className="max-w-[1240] mx-auto px-2">
-        <p className="text-xl tracking-widest uppercase text-[#21568a]">
-          Projects
-        </p>
-        <h2 className="py-4">What I&apos;ve Built</h2>
-        <p className="text-xs text-center sm:text-left sm:text-base tracking-widest uppercase py-4">
-          Click on the images below to view additional details
-        </p>
-        <div className="grid md:grid-cols-2 gap-8">
-          <ProjectItem
-            title="Ink Buzz"
-            backgroundImg={inkImg}
-            projectUrl="/inkBuzzProject"
-            description="A social media platform where users can connect with tattoo artists"
-          />
-          <ProjectItem
-            title="Poké Mart"
-            backgroundImg={pokeImg}
-            projectUrl="/pokeProject"
-            description="An e-commerce website based on the in-game marts found in the Pokémon series"
-          />
-          <ProjectItem
-            title="Tiger Sports Basketball League"
-            backgroundImg={tigerImg}
-            projectUrl="/tigerProject"
-            description="A league website for players, coaches, and administrators"
-          />
+    <div id="projects" className="w-full p-2 flex items-center pt-8 mx-5">
+      <div className="max-w-[1240px] m-auto px-2">
+        <div>
+          <p className="uppercase text-xl tracking-widest text-[#21568a]">
+            Projects
+          </p>
+          <h2 className="py-4">What I&apos;ve Built</h2>
+          <p className="text-xs text-center sm:text-left sm:text-base tracking-widest uppercase py-4">
+            Click on the images below to view additional details
+          </p>
+          <div className="grid md:grid-cols-2 gap-8">
+            <ProjectItem
+              title="Ink Buzz"
+              backgroundImg={inkImg}
+              projectUrl="/inkBuzzProject"
+              description="A social media platform where users can connect with tattoo artists"
+            />
+            <ProjectItem
+              title="Poké Mart"
+              backgroundImg={pokeImg}
+              projectUrl="/pokeProject"
+              description="An e-commerce website based on the in-game marts found in the Pokémon series"
+            />
+            <ProjectItem
+              title="Tiger Sports Basketball League"
+              backgroundImg={tigerImg}
+              projectUrl="/tigerProject"
+              description="A league website for players, coaches, and administrators"
+            />
+          </div>
         </div>
       </div>
     </div>
