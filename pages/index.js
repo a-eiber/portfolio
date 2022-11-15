1f41cfc9b4127b27a645f1c4c83e1cf79c71e239
Alec Eiber <aleceiber@gmail.com>
2022-11-15 10:32:38 -0500

    removed posts

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import About from '../components/About';
 import Contact from '../components/Contact';
 import Main from '../components/Main';
-import Posts from '../components/Posts';
+// import Posts from '../components/Posts';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
 
@@ -23,7 +23,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
-      <Posts />
+      {/* <Posts /> */}
       <Contact />
     </div>
   );
