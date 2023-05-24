db32d622414a5ea54d1f4611336277eb4e7b021e
Alec Eiber <aleceiber@gmail.com>
2023-05-24 14:43:06 -0400

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
+
 
 function MyApp({ Component, pageProps }) {
+  <Analytics />
   return <Component {...pageProps} />;
 }
 
